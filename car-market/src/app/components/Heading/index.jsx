e377acaddeaf3315abae0f6b8a1c92387d5ad866
Alisher <alisherxujanov163@gmail.com>
2025-09-09 18:15:58 +0500

    useState started

--- car-market/src/app/components/Heading/index.jsx
+++ car-market/src/app/components/Heading/index.jsx
@@ -1,17 +1,34 @@
+"use client"
+
 import "./style.scss";
+import { useState } from 'react'
 
+// HOOK  =>  is special ability that every component can use
+// 
+// useState  =>  [use=использовать,  state=состояние-память]
+// each useState is unique for each component
 
-function Heading(props) {
+// RULES:
+// 1. Always import it from 'react' inside {...}
+// 2. They should be initialized inside the component in top level
+// 3. [useState]  =>  const [var, setVar] = useState(initialValue)
+
+const Heading = ({ color, children }) => { // destructuring
+    const [currentColor, setCurrentColor] = useState("red")
     // props  =>  properties  =>  это объект
     // props === { title: 'About page' }
-
+    function updateColor(e) {
+        setCurrentColor(e.target.value)
+    }
     return (
-        <h1 id="main-heading" style={{ color: props.color }}>
-            { props.children }
+        <h1 id="main-heading" style={{ color: currentColor }}>
+            {children}
+            <input type="color" onChange={updateColor} value={currentColor}/>
         </h1>
     );
 }
 
+// export const x = ({ a, b }) => {
+//     return a + b
+// }
 export default Heading;
-
-
